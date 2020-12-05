e52c4e663c2c745a5e75861d3e1d82ca0ff6f60e
Umidbek <umka_warface@mail.ru>
2020-12-05 10:18:38 +0500

    backend start

---
+++ backend/apps/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=True)
+    updated_at = models.DateTimeField(auto_now=True, editable=False, null=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('id',)
