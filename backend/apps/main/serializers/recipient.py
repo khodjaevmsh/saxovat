bc5843e65232a9f91e2d6c05c79a77928c059e0b
UmidbeU <umka_warface@mail.ru>
2020-12-05 21:09:59 +0500

    fix api

--- backend/apps/main/serializers/recipient.py
+++ backend/apps/main/serializers/recipient.py
@@ -6,4 +6,4 @@ from main.models import Recipient
 class RecipientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipient
-        fields = ('id', 'category', 'phone', 'address',)
+        fields = ('id', 'name', 'phone', 'comment', 'category',)
